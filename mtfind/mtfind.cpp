f656b079667c909baef3d44bea677f7b689a4a58
Iaroslav <IaroslavInternational@yandex.ru>
2022-09-11 15:32:41 +0300

    Добавлена функция чтения файла + тест

--- mtfind/mtfind.cpp
+++ mtfind/mtfind.cpp
@@ -1,6 +1,33 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
-int main()
-{    
+void ReadFile(const std::string& adr, std::vector<std::string>* lines)
+{
+    std::ifstream file(adr); 
+    std::string s;
+
+    while (std::getline(file, s)) 
+    { 
+        lines->push_back(s);
+    }
+
+    file.close();
+
+    lines->shrink_to_fit();
+}
+
+int main(int argc, char* argv[])
+{   
+    std::vector<std::string> lines;
+
+    ReadFile(argv[1], &lines);
+
+    for (auto& s : lines)
+    {
+        std::cout << s << std::endl;
+    }
+
     return 0;
 }
